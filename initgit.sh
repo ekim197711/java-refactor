13f40f8237b17ee04c162051c97c05f034b09db5
Mike Møller Nielsen <mmni@protonmail.com>
2020-02-22 21:21:54 +0100

    files

---
+++ initgit.sh
@@ -0,0 +1,6 @@
+echo "# java-refactor" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:ekim197711/java-refactor.git
+git push -u origin master
---
+++ src/Spacecraft.java
@@ -0,0 +1,12 @@
+public class Spacecraft {
+
+    private final String buzzFlyyyyy;
+
+    public Spacecraft(String flyyyyy) {
+        buzzFlyyyyy = flyyyyy;
+    }
+
+    public void fly(){
+        System.out.println(buzzFlyyyyy);
+    }
+}
